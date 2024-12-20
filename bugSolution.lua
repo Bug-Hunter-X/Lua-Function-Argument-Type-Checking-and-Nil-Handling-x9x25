2cd2145b9f3eaaf02523a1b27a15345bad51481a
BugHunter-X <askzenai@gmail.com>
2024-12-20 16:02:06 +0100

    Add bugSolution.lua file

---
+++ bugSolution.lua
@@ -0,0 +1,17 @@
+local function foo(x)
+  if x == nil then
+    return 0 -- or handle nil as appropriate for your function's logic
+  elseif type(x) ~= 'number' then
+    error('Invalid argument: x must be a number')
+  else
+    return x * 2
+  end
+end
+
+local result = foo('hello') -- Throws an error as expected
+
+local result2 = foo(nil) -- returns 0
+print(result2)
+
+local result3 = foo(10) -- returns 20 as expected
+print(result3) 
